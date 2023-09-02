6282fa1877879d2fb69eda1178c06acdea9b7a32
Priyanshu2662 <91813367+Priyanshu2662@users.noreply.github.com>
2023-09-02 21:35:47 +0000

    further learnings

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,6 @@
-console.log("priyanshu saxena")
+const accountId = 2302323
+let accountEmail = "priyanshusaxena.ahm123@gmail.com"
+var accountpassword = "122334"
+accountcity = "Jaipur"
+
+console.table([accountEmail, accountId, accountpassword, accountcity])
